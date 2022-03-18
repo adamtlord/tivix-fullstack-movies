e5851bd96374e0864b473d3534866da41fd49773
Adam Lord <adam@tivix.com>
2022-03-18 15:37:29 -0700

    Downgrade again, add env vars to next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org']
   },
-  webpack5:false
+  webpack5:false,
+  env: {
+    AMPLIFY_NEXTJS_EXPERIMENTAL_TRACE: process.env.AMPLIFY_NEXTJS_EXPERIMENTAL_TRACE,
+    THEMOVIEDB_API_KEY: process.env.THEMOVIEDB_API_KEY,
+    THEMOVIEDB_API_URL: process.env.THEMOVIEDB_API_URL,
+    _LIVE_UPDATES: process.env._LIVE_UPDATES,
+  }
 }
 
 module.exports = nextConfig
